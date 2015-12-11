97119597658c78ceea1905bee145dc9c4d3b2026
bjuppa <nilsved@gmail.com>
2015-12-11 12:03:30 +0100

    PHPUnit setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,10 +16,20 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      */
     public function createApplication()
     {
-        $app = require __DIR__.'/../bootstrap/app.php';
+        $app = require __DIR__ . '/../bootstrap/app.php';
 
         $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
+        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.connections.sqlite.database', ':memory:');
+
         return $app;
     }
+
+    public function setUp()
+    {
+        parent::setUp();
+        Artisan::call('migrate');
+    }
+
 }
