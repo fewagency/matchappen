2319c1516b2c21eebb98c2fccb99bd65a3117d01
bjuppa <nilsved@gmail.com>
2015-12-21 16:33:09 +0100

    basic time related inputs

--- resources/views/opportunity/partials/edit_form.blade.php
+++ resources/views/opportunity/partials/edit_form.blade.php
@@ -12,15 +12,35 @@
   @endif
 
   <div>
-    Beskrivning
-    <textarea name="description">{{ old('description', $opportunity->description) }}</textarea>
-  </div>
-
-  <div>
     Antal platser
     <input type="number" name="max_visitors" min="1" max="{{ \Matchappen\Opportunity::MAX_VISITORS }}"
            value="{{ old('max_visitors', $opportunity->max_visitors) }}">
   </div>
+
+  <div>
+    Start
+    <input type="datetime-local" name="start" value="{{ old('start', $opportunity->start->toW3cString()) }}">
+  </div>
+
+  <div>
+    Längd
+    <select name="minutes">
+      @foreach(trans('opportunity.minutes_options') as $minutes => $string)
+        <option value="{{ $minutes }}" @if($minutes == $opportunity->minutes) selected @endif >{{ $string }}</option>
+      @endforeach
+    </select>
+  </div>
+
+  <div>
+    Anmälan fram till
+    <input type="datetime-local" name="registration_end" value="{{ old('registration_end', $opportunity->registration_end->toW3cString()) }}">
+  </div>
+
+  <div>
+    Beskrivning
+    <textarea name="description">{{ old('description', $opportunity->description) }}</textarea>
+  </div>
+
 
   <div>
     <button type="submit">Spara</button>
