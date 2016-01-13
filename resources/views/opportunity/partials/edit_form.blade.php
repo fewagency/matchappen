d2365c7816876f443e565fa4c21213f11595cdcc
bjuppa <nilsved@gmail.com>
2016-01-13 16:07:24 +0100

    default form values when creating opportunity

--- resources/views/opportunity/partials/edit_form.blade.php
+++ resources/views/opportunity/partials/edit_form.blade.php
@@ -1,3 +1,4 @@
+@inject('carbon', '\Carbon\Carbon')
 <form method="POST"
       action="{{ $opportunity->exists() ? action('OpportunityController@update', $opportunity->getKey()) : action('OpportunityController@store') }}"
 >
@@ -14,12 +15,12 @@
   <div>
     Antal platser
     <input type="number" name="max_visitors" min="1" max="{{ \Matchappen\Opportunity::MAX_VISITORS }}"
-           value="{{ old('max_visitors', $opportunity->max_visitors) }}">
+           value="{{ old('max_visitors', $opportunity->max_visitors ?: 5) }}">
   </div>
 
   <div>
     Start
-    <input type="datetime-local" name="start" value="{{ old('start', $opportunity->start->toW3cString()) }}">
+    <input type="datetime-local" name="start" value="{{ old('start', $carbon->parse($opportunity->start ?: $carbon->parse('+30 weekdays 15:00'))->toW3cString()) }}">
   </div>
 
   <div>
@@ -33,7 +34,7 @@
 
   <div>
     Anmälan fram till
-    <input type="datetime-local" name="registration_end" value="{{ old('registration_end', $opportunity->registration_end->toW3cString()) }}">
+    <input type="datetime-local" name="registration_end" value="{{ old('registration_end', $carbon->parse($opportunity->registration_end ?: $carbon->parse('+20 weekdays'))->toW3cString()) }}">
   </div>
 
   <div>
