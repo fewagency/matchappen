902b3dffec66cf6b829131f9793e98315f167365
bjuppa <nilsved@gmail.com>
2016-01-14 12:06:04 +0100

    contact name min length

--- app/Http/Requests/StoreOpportunityRequest.php
+++ app/Http/Requests/StoreOpportunityRequest.php
@@ -58,7 +58,7 @@ class StoreOpportunityRequest extends Request
                 implode(',', array_keys(trans('opportunity.minutes_options'))),
             'registration_end' => 'string|required_with:start',
             'address' => 'string|max:400',
-            'contact_name' => 'string|max:100',
+            'contact_name' => 'string|min:5|max:100',
             'contact_phone' => ['string', 'max:20', 'regex:'.trans('general.local_phone_regex')],
         ];
     }
