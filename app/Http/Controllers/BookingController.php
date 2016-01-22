aa63e12cf6572c57c3b085b58ce823009434e461
bjuppa <nilsved@gmail.com>
2016-01-22 16:25:49 +0100

    cancel booking

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -23,7 +23,7 @@ class BookingController extends Controller
     public function store(Opportunity $opportunity, StoreBookingRequest $request, EmailTokenGuard $guard)
     {
         if (!$opportunity->isBookable()) {
-            return redirect(action('OpportunityController@show', $opportunity));
+            return redirect()->action('OpportunityController@show', $opportunity);
         }
         $booking = new Booking($request->input());
         $booking->opportunity()->associate($opportunity);
@@ -32,7 +32,7 @@ class BookingController extends Controller
 
             //TODO: email booking link to supervisor
 
-            return redirect(action('BookingController@show', $booking));
+            return redirect()->action('BookingController@show', $booking);
         } else {
             $booking->reserved_until = Carbon::parse('+1 hour');
             $booking->save();
@@ -40,7 +40,7 @@ class BookingController extends Controller
 
             //TODO: email token to pupil
 
-            return redirect(action('BookingController@reserved'))->with('reserved_booking_id', $booking->getKey());
+            return redirect()->action('BookingController@reserved')->with('reserved_booking_id', $booking->getKey());
         }
     }
 
@@ -48,7 +48,7 @@ class BookingController extends Controller
     {
         $booking = Booking::find($request->session()->get('reserved_booking_id'));
         if (empty($booking)) {
-            return redirect(action('OpportunityController@index'));
+            return redirect()->action('OpportunityController@index');
         }
 
         return view('booking.reserved', compact('booking'));
@@ -74,8 +74,13 @@ class BookingController extends Controller
 
     public function postCancel(Booking $booking, CancelBookingRequest $request)
     {
-        //TODO: cancel booking (soft-delete)
+        //Cancel booking (soft-delete)
+        $booking->delete();
+
         //TODO: notify supervisor
-        //TODO: redirect to user's list of bookings, a dashboard, with a status message
+
+        return redirect()->route('dashboard')->with('status',
+            trans('booking.cancelled', ['booking' => $booking->opportunity->name])
+        );
     }
 }
