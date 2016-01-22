22e0df9566907b5968fcdd1c1bc7eebf10a75ae4
bjuppa <nilsved@gmail.com>
2016-01-22 16:33:31 +0100

    dashboard for supervisors and students too

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,26 +6,34 @@ use Illuminate\Http\Request;
 
 use Matchappen\Http\Requests;
 use Matchappen\Http\Controllers\Controller;
+use Matchappen\Services\EmailTokenGuard;
 use Matchappen\Workplace;
 
 class UserController extends Controller
 {
-    public function dashboard(Request $request)
+    public function dashboard(Request $request, EmailTokenGuard $token_guard)
     {
+        if ($token_guard->check()) {
+            if ($token_guard->checkSupervisor()) {
+                return view('user.dashboard_supervisor');
+            } else {
+                return view('user.dashboard_student');
+            }
+        }
+
         $user = $request->user();
+        if (!$user) {
+            return redirect()->action('Auth\AuthController@getLogin');
+        }
 
         if ($user->is_admin) {
-
             return view('user.dashboard_admin')->with([
                 'user' => $user,
                 'new_workplaces' => Workplace::publishRequested()->get(),
                 'unpublished_workplaces' => Workplace::unpublished()->get(),
             ]);
-
         } else {
-
             return view('user.dashboard_workplace')->with(['user' => $user, 'workplace' => $user->workplace]);
-
         }
     }
 }
