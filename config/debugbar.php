52fa181e14fd188e02c6849344a41c506654238d
bjuppa <nilsved@gmail.com>
2016-02-08 14:27:16 +0100

    disabled debugbar from pulling in own jquery

---
+++ config/debugbar.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+
+    /*
+     |--------------------------------------------------------------------------
+     | Vendors
+     |--------------------------------------------------------------------------
+     |
+     | Vendor files are included by default, but can be set to false.
+     | This can also be set to 'js' or 'css', to only include javascript or css vendor files.
+     | Vendor files are for css: font-awesome (including fonts) and highlight.js (css files)
+     | and for js: jquery and and highlight.js
+     | So if you want syntax highlighting, set it to true.
+     | jQuery is set to not conflict with existing jQuery scripts.
+     |
+     */
+
+    'include_vendors' => 'css',
+
+);
