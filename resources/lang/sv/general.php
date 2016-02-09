f536aca07ace6ab462f7fae156a126accc8b2cf5
bjuppa <nilsved@gmail.com>
2016-02-09 10:52:16 +0100

    fixed broken validation regexps

--- resources/lang/sv/general.php
+++ resources/lang/sv/general.php
@@ -8,6 +8,6 @@ return [
     'edu-staff' => 'skolpersonal',
     'student' => 'student|studenter',
 
-    'local_phone_regex' => ' /^(\+46 ?|0)[1 - 9]\d ? -?(\d ?){5,}$/',
-    'personal_name_regex' => ' /^.+\s . +/',
+    'local_phone_regex' => '/^(\+46 ?|0)[1-9]\d?-?(\d ?){5,}$/',
+    'personal_name_regex' => '/^.+\s.+/',
 ];
