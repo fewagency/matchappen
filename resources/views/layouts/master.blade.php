401e4d6bee45f37e950cd1c046de5675f098a427
folbert <b@folbert.com>
2016-02-15 11:53:38 +0100

    Attempt to improve the perception of speed
    Added some

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,16 +1,48 @@
 <!DOCTYPE html>
-<html lang="{{ App::getLocale() }}" class="no-js">
+<html lang="{{ App::getLocale() }}">
 <head>
   <meta charset="utf-8">
-  <script>document.documentElement.className += ' wf-loading';</script>
+
+  <style>
+
+    body {
+      background: #000;
+    }
+
+    .js body.loading {
+      height: 100vh;
+      width: 100vw;
+      overflow: hidden;
+    }
+
+    .js body.loading > * {
+      visibility: hidden;
+    }
+
+    /*
+    .js body.loading > #loading-indicator {
+      display: block;
+      visibility: visible;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      margin: -71px 0 0 -71px;
+    }
+    */
+
+  </style>
+
   <meta content='width=device-width, initial-scale=1.0' name='viewport'/>
   <title>{{ $title or trans('general.appname') }} – Malmö stad</title>
   <!--[if IE]><meta content='IE=edge' http-equiv='X-UA-Compatible'/><![endif]-->
   <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
   <script src="{{ asset('/build/js/modernizr.js') }}"></script> {{-- Lets put modernizr here since some CSS relies heavily on it --}}
   <script src="https://use.typekit.net/kjy7xgd.js"></script>
-  <script>try{Typekit.load({ async: true });}catch(e){}</script>
-
+  <script>
+    try{Typekit.load({ async: true });}catch(e){};
+    document.getElementsByTagName('HTML')[0].classList.add('js');
+    document.documentElement.className += ' wf-loading';
+  </script>
 </head>
 <body class="loading {{ $body_class or '' }}">
 
@@ -20,10 +52,8 @@
 @yield('content')
 
 
-@include('partials.loading-indicator');
+{{-- @include('partials.loading-indicator') --}}
 
-{{-- <script src='//assets.malmo.se/external/v4/malmo.js'></script> --}}
 <script src='{{ elixir('js/all.js') }}'></script>
-<!--<script src='/your_own_javascript.js'></script>-->
 </body>
 </html>
