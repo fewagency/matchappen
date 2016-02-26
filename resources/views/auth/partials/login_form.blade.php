7126a3f7974f60c1683eaebc5d0d2b7c8a875df8
bjuppa <nilsved@gmail.com>
2016-02-26 15:17:32 +0100

    Now using fluent-form for login form

--- resources/views/auth/partials/login_form.blade.php
+++ resources/views/auth/partials/login_form.blade.php
@@ -1,25 +1,7 @@
-<form method="POST" action="{{ action('Auth\AuthController@postLogin') }}">
-  {!! csrf_field() !!}
-
-  @if (count($errors) > 0)
-    <ul>
-      @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-      @endforeach
-    </ul>
-  @endif
-
-  <div>
-    Email
-    <input type="email" name="email" value="{{ old('email') }}">
-  </div>
-
-  <div>
-    Password
-    <input type="password" name="password" id="password">
-  </div>
-
-  <div>
-    <button type="submit">Login</button>
-  </div>
-</form>
+{!!
+FluentForm::withAction(action('Auth\AuthController@postLogin'))->withValues(old())->withErrors($errors->getMessageBag())
+->withToken(csrf_token())
+->containingInputBlock('email','email')->withLabel('Din epostadress')
+->followedByPasswordBlock('password')->withLabel('Ditt lösenord')
+->followedByButtonBlock('Login')
+!!}
