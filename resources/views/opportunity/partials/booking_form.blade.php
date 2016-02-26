4a4c304323deac5aa0e898426e7a56cc52814061
bjuppa <nilsved@gmail.com>
2016-02-26 16:27:05 +0100

    fluent-form for booking form

--- resources/views/opportunity/partials/booking_form.blade.php
+++ resources/views/opportunity/partials/booking_form.blade.php
@@ -6,24 +6,30 @@
   ->withValues(old())
   ->withErrors($errors)
   ->withToken(csrf_token())
+
   ->containingInputBlock('name')
   ->required()
+
   ->followedByInputBlock('visitors','number')
   ->onlyDisplayedIf($token_guard->checkSupervisor())
   ->withInputAttribute('max',$opportunity->placesLeft())
   ->required()
+
   ->followedByInputBlock('email', 'email')
   ->withLabel($token_guard->checkSupervisor() ? 'Elevens epost' : trans('auth.labels.your_email'))
   ->required(!$token_guard->checkSupervisor())
   ->withDescription($token_guard->checkSupervisor() ? '(om du bokar till 1 elev)' : null)
+
   ->followedByInputBlock('supervisor_email','email')
   ->readonly($token_guard->checkSupervisor())
   ->required()
   ->withLabel($token_guard->checkSupervisor() ? trans('auth.labels.your_email') : 'Din lärares epostadress')
   ->withDescription($token_guard->checkSupervisor() ? '' : '(mentor eller SYV går också bra)')
+
   ->followedByInputBlock('phone','tel')
   ->withLabel('Ditt mobilnummer')
   ->withDescription('(frivilligt)')
+
   ->followedByButtonBlock('Boka')
 !!}
 <!-- TODO: lägg till årskurs i bokningsformuläret -->
