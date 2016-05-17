3d0ce49e437287a9cf5331105e540c88f9edc20e
bjuppa <nilsved@gmail.com>
2016-05-17 14:32:55 +0200

    Behat setup

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,31 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+use Laracasts\Behat\Context\DatabaseTransactions;
+use Laracasts\Behat\Context\Migrator;
+use Laracasts\Behat\Context\Services\MailTrap;
+use PHPUnit_Framework_Assert as PHPUnit;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
+{
+    use DatabaseTransactions, Migrator;
+    use MailTrap;
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+}
