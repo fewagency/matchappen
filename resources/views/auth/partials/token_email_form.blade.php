6cbcfc36735d776a32597636936c107152557ca7
bjuppa <nilsved@gmail.com>
2016-05-18 17:18:47 +0200

    upgraded token email form

--- resources/views/auth/partials/token_email_form.blade.php
+++ resources/views/auth/partials/token_email_form.blade.php
@@ -1,6 +1,6 @@
-{!!
+{{
 FluentForm::withAction(action('Auth\EmailTokenController@postEmail'))->withValues(old())->withErrors($errors)
 ->withToken(csrf_token())
-->containingInputBlock('email','email')->withLabel(trans('auth.labels.your_email'))
+->containingInputBlock('email','email')->withLabel(trans('auth.labels.your_email'))->required()
 ->followedByButtonBlock(trans('auth.actions.login'))
-!!}
+}}
