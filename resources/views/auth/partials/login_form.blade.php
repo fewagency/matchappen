9a16258b072ec67aa7500b96379d2659c4927163
bjuppa <nilsved@gmail.com>
2016-05-18 17:09:23 +0200

    upgraded login form

--- resources/views/auth/partials/login_form.blade.php
+++ resources/views/auth/partials/login_form.blade.php
@@ -1,7 +1,7 @@
-{!!
+{{
 FluentForm::withAction(action('Auth\AuthController@postLogin'))->withValues(old())->withErrors($errors)
 ->withToken(csrf_token())
-->containingInputBlock('email','email')->withLabel(trans('auth.labels.your_email'))
-->followedByPasswordBlock()->withLabel(trans('auth.labels.your_password'))
+->containingInputBlock('email','email')->withLabel(trans('auth.labels.your_email'))->required()
+->followedByPasswordBlock()->withLabel(trans('auth.labels.your_password'))->required()
 ->followedByButtonBlock(trans('auth.actions.login'))
-!!}
+}}
