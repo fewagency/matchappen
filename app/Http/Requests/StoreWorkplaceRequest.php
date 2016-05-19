8eeed9c0a2b99585038b29eb99cb89ae6f6470d2
bjuppa <nilsved@gmail.com>
2016-05-19 13:42:14 +0200

    refactored workplace validation

--- app/Http/Requests/StoreWorkplaceRequest.php
+++ app/Http/Requests/StoreWorkplaceRequest.php
@@ -36,28 +36,37 @@ class StoreWorkplaceRequest extends Request
 
     public static function rulesForCreate()
     {
-        $rules = self::rulesForUpdate();
-        $rules['name'] .= '|required';
-        $rules['employees'] .= '|required';
-        $rules['address'] .= '|required';
+        $rules = [
+            'name' => ['required', 'min:3', 'max:255', 'unique:workplaces,name'],
+            'employees' => ['required', 'integer', 'min:1', 'max:65535'],
+            'description' => ['string'],
+            'homepage' => ['url', 'max:255'],
+            'contact_name' => ['string', 'max:100', 'regex:' . trans('general.personal_name_regex')],
+            'email' => ['email', 'max:50'],
+            'phone' => ['string', 'max:20', 'regex:' . trans('general.local_phone_regex')],
+            'address' => ['required', 'string', 'max:500'],
+        ];
 
         return $rules;
     }
 
     public static function rulesForUpdate($exclude_id_from_unique = null)
     {
-        $rules = [
-            'name' => 'min:3|max:255|unique:workplaces,name',
-            'employees' => 'integer|min:1|max:65535',
-            'description' => 'string',
-            'homepage' => 'url|max:255',
-            'contact_name' => ['string', 'max:100', 'regex:' . trans('general.personal_name_regex')],
-            'email' => 'email|max:50',
-            'phone' => ['string', 'max:20', 'regex:' . trans('general.local_phone_regex')],
-            'address' => 'string|max:500',
-        ];
-        if ($exclude_id_from_unique) {
-            $rules['name'] = $rules['name'] .= ',' . $exclude_id_from_unique;
+        $rules = self::rulesForCreate();
+
+        foreach ($rules as $field => &$field_rules) {
+
+            // Handle unique-rules
+            if ($exclude_id_from_unique) {
+                foreach ($field_rules as &$rule) {
+                    if (starts_with($rule, 'unique:workplaces,')) {
+                        $rule .= ',' . $exclude_id_from_unique;
+                    }
+                }
+            }
+
+            // Make all rules apply only if field is present
+            array_unshift($field_rules, 'sometimes');
         }
 
         return $rules;
