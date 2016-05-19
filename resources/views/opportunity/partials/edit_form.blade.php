ce8828fda464e8fa81e770b5ab58206503e97320
bjuppa <nilsved@gmail.com>
2016-05-19 14:28:19 +0200

    more todo

--- resources/views/opportunity/partials/edit_form.blade.php
+++ resources/views/opportunity/partials/edit_form.blade.php
@@ -34,7 +34,7 @@ FluentForm::withAction($opportunity->exists ? action('OpportunityController@upda
 ->followedByInputBlock('description', 'textarea')
 
 ->followedByInputBlock('address', 'textarea')
-//TODO: this replace of newlines may not be needed
+//TODO: this replace of newlines may not be needed if handled in FluentHtml
 ->withInputAttribute('placeholder', str_replace("\n", " ", $opportunity->fallback_address))
 
 ->followedByInputBlock('contact_name')
