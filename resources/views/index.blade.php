2f35a6e220dcf3563d5c1f1a033cb684e652df99
bjuppa <nilsved@gmail.com>
2016-05-25 16:17:38 +0200

    text on index page + hiding empty sections

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,61 +5,70 @@
   @include('partials.text-block-1', [
     'headline_level' => 'h1',
     'headline' => 'Hej!',
-    'text' => '<p>Lorem ipsum dolor sit amet, <b>consectetur adipiscing elit</b>, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+    'text' =>
+    'Här hittar du intressanta yrken och arbetsplatser i Malmö och kan boka möten till studiebesök.
+<br>
+Om du vill erbjuda studiebesök på din arbetsplats kan du <a href="'.action('Auth\AuthController@getLogin').'">logga in här</a>.'
   ])
 
-  @include('partials.mega-nav-item', [
-    'main_href' => action('WorkplaceController@index'),
-    'item_modifiers' => ['companies'],
-    'item_id' => 'companies',
-    'use_fold_effect' => true,
-    'cta_icon' => trans('assets.workplaces-icon'),
-    'headline' => 'Arbetsplatser',
-    'headline_level' => 2,
-    'intro_text' => 'Läs om lorem ipsum dolor sit amet',
-    'sub_items' => $workplaces,
-    'extra_sub_items' => [
-      [
-        'href' => action('WorkplaceController@index'),
-        'text' => 'Se fler '.trans_choice('workplace.workplace', 2)
-      ]
-    ]
-    ])
-
-  @include('partials.mega-nav-item', [
-    'main_href' => action('OccupationController@index'),
-    'item_modifiers' => ['occupations'],
-    'item_id' => 'occupations',
-    'use_fold_effect' => true,
-    'cta_icon' => trans('assets.occupations-icon'),
-    'headline' => 'Yrken',
-    'headline_level' => 2,
-    'intro_text' => 'Läs om lorem ipsum dolor sit amet',
-    'sub_items' => $occupations,
-    'extra_sub_items' => [
-      [
-        'href' => action('OccupationController@index'),
-        'text' => 'Se fler '.trans_choice('occupation.occupation', 2)
-      ]
-    ]
-  ])
-
-  @include('partials.mega-nav-item', [
-      'main_href' => action('OpportunityController@index'),
-      'item_modifiers' => ['opportunities'],
-      'item_id' => 'opportunities',
+  @if(count($workplaces))
+    @include('partials.mega-nav-item', [
+      'main_href' => action('WorkplaceController@index'),
+      'item_modifiers' => ['companies'],
+      'item_id' => 'companies',
       'use_fold_effect' => true,
-      'cta_icon' => trans('assets.opportunities-icon'),
-      'headline' => trans_choice('opportunity.opportunity',2),
+      'cta_icon' => trans('assets.workplaces-icon'),
+      'headline' => 'Arbetsplatser',
       'headline_level' => 2,
-      'intro_text' => 'Läs om lorem ipsum dolor sit amet',
-      'sub_items' => $opportunities,
+      'intro_text' => 'Kolla vilka arbetsplatser du kan besöka.',
+      'sub_items' => $workplaces,
       'extra_sub_items' => [
         [
-          'href' => action('OpportunityController@index'),
-          'text' => 'Se fler '.trans_choice('opportunity.opportunity', 2)
+          'href' => action('WorkplaceController@index'),
+          'text' => 'Se fler '.trans_choice('workplace.workplace', 2)
+        ]
+      ]
+      ])
+  @endif
+
+  @if(count($occupations))
+    @include('partials.mega-nav-item', [
+      'main_href' => action('OccupationController@index'),
+      'item_modifiers' => ['occupations'],
+      'item_id' => 'occupations',
+      'use_fold_effect' => true,
+      'cta_icon' => trans('assets.occupations-icon'),
+      'headline' => 'Yrken',
+      'headline_level' => 2,
+      'intro_text' => 'Bläddra bland yrken.',
+      'sub_items' => $occupations,
+      'extra_sub_items' => [
+        [
+          'href' => action('OccupationController@index'),
+          'text' => 'Se fler '.trans_choice('occupation.occupation', 2)
         ]
       ]
     ])
+  @endif
+
+  @if(count($opportunities))
+    @include('partials.mega-nav-item', [
+        'main_href' => action('OpportunityController@index'),
+        'item_modifiers' => ['opportunities'],
+        'item_id' => 'opportunities',
+        'use_fold_effect' => true,
+        'cta_icon' => trans('assets.opportunities-icon'),
+        'headline' => trans_choice('opportunity.opportunity',2),
+        'headline_level' => 2,
+        'intro_text' => 'Boka in ditt studiebesök.',
+        'sub_items' => $opportunities,
+        'extra_sub_items' => [
+          [
+            'href' => action('OpportunityController@index'),
+            'text' => 'Se fler '.trans_choice('opportunity.opportunity', 2)
+          ]
+        ]
+      ])
+  @endif
 
 @endsection
