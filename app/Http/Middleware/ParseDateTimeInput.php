ff320a9dab9ab756911671b659468ad9db3997ea
bjuppa <nilsved@gmail.com>
2016-07-08 11:50:00 +0200

    Combine _date and _time fields into request input

--- app/Http/Middleware/ParseDateTimeInput.php
+++ app/Http/Middleware/ParseDateTimeInput.php
@@ -6,7 +6,9 @@ use Carbon\Carbon;
 use Closure;
 
 /*
- * Parses input data in the request through Carbon datetime.
+ * Parses input data in the request through Carbon datetime and puts a valid datetime string back into the input data.
+ * Also combines fields suffixed with _date and _time into the main field if conditions are met.
+ *
  * Define fields with : and comma-separated list of fieldnames (dot-notated).
  *
  * Example for route definition:
@@ -24,20 +26,30 @@ class ParseDatetimeInput
      *
      * @param  \Illuminate\Http\Request $request
      * @param  \Closure $next
-     * @param string ,... $date_field
+     * @param string ,... $datetime_field
      * @return mixed
      */
-    public function handle($request, Closure $next, $date_field)
+    public function handle($request, Closure $next, $datetime_field)
     {
-        $input = $request->input();
-        foreach (array_slice(func_get_args(), 2) as $date_field) {
-            if (array_has($input, $date_field)) {
+        $input = $request->input(); // The whole input array is pulled out to access and modify it using dot-notation, then put it back into the request when done
+        foreach (array_slice(func_get_args(), 2) as $datetime_field) {
+            $date_field = $datetime_field . '_date';
+            $time_field = $datetime_field . '_time';
+            if (array_has($input, $datetime_field)) {
+                // The field is present
+                // Try to parse it
                 try {
-                    $carbon = Carbon::parse(array_get($input, $date_field));
-                    array_set($input, $date_field, $carbon->toDateTimeString());
-                } catch(\Exception $e) {
-                    // If parse fails, do nothing to the input
+                    $carbon = Carbon::parse(array_get($input, $datetime_field));
+                    array_set($input, $datetime_field, $carbon->toDateTimeString());
+                } catch (\Exception $e) {
+                    // The parse failed
+                    // Do nothing to the input
                 }
+            } elseif (array_has($input, $date_field) and array_has($input, $time_field)) {
+                // The field is not present but the date and time fields are
+                // Combine the date and time parts into the field
+                array_set($input, $datetime_field,
+                    array_get($input, $date_field) . ' ' . array_get($input, $time_field));
             }
         }
         $request->replace($input);
