9268327fbc5d751b6a12f881badba4f257afd084
bjuppa <nilsved@gmail.com>
2016-08-18 11:00:03 +0200

    added email prefill back in to token log in form

--- resources/views/auth/partials/token_login_form.blade.php
+++ resources/views/auth/partials/token_login_form.blade.php
@@ -1,5 +1,5 @@
 {{
-FluentForm::withAction(action('Auth\EmailTokenController@postLogin', $token))->withValues(old())->withErrors($errors)
+FluentForm::withAction(action('Auth\EmailTokenController@postLogin', $token))->withValues(compact('email'),old())->withErrors($errors)
 ->withToken(csrf_token())
 ->containingInputBlock('email','email')->withLabel(trans('auth.labels.your_email'))->required()
 ->followedByButtonBlock(trans('auth.actions.login'))
