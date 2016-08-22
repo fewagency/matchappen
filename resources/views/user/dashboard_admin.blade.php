65f212c5c853cd873a9d9507ea797a05a249adbf
bjuppa <nilsved@gmail.com>
2016-08-22 13:12:49 +0200

    clarified admin dashboard

--- resources/views/user/dashboard_admin.blade.php
+++ resources/views/user/dashboard_admin.blade.php
@@ -2,10 +2,16 @@
 
 @section('content')
 
-  <h2>Opublicerade {{ trans_choice('workplace.workplace', 2) }} att godkänna</h2>
-  @include('workplace.partials.list', ['workplaces' => $new_workplaces])
+  <h1>Välkommen admin!</h1>
 
-  <h2>Opublicerade {{ trans_choice('workplace.workplace', 2) }}</h2>
-  @include('workplace.partials.list', ['workplaces' => $unpublished_workplaces])
+  <p>Här på din hemsida listas opublicerade arbetsplatser. Alla publicerade arbetsplatser kan du hitta och redigera genom <a href="{{ action('WorkplaceController@index') }}">själva webbsidan</a>.</p>
+
+  @if(count($new_workplaces))
+    @include('workplace.partials.list', ['workplaces' => $new_workplaces, 'headline' => 'Opublicerade '.trans_choice('workplace.workplace', 2).' att godkänna', 'intro_text' => 'Dessa '.trans_choice('workplace.workplace', 2).' ska granskas och godkännas för att publiceras.'])
+  @endif
+
+  @if(count($unpublished_workplaces))
+    @include('workplace.partials.list', ['workplaces' => $unpublished_workplaces, 'headline' => 'Opublicerade '. trans_choice('workplace.workplace', 2), 'intro_text' => 'Dessa '.trans_choice('workplace.workplace', 2).' är ej publika. Det kan vara för att du som admin inte godkände dem, eller för att arbetsplatsen själv valt att avpublicera sin information.'])
+  @endif
 
 @endsection
