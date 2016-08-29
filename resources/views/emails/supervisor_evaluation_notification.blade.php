4d2a96da0c3905bf71bb38fbf092643d1b6c9954
bjuppa <nilsved@gmail.com>
2016-08-29 11:40:20 +0200

    Add contact details to evaluation notification

--- resources/views/emails/supervisor_evaluation_notification.blade.php
+++ resources/views/emails/supervisor_evaluation_notification.blade.php
@@ -1,4 +1,16 @@
-Betyg: {{ $evaluation->rating }}
+<div>
+  Betyg: {{ $evaluation->rating }}
+</div>
 @if($evaluation->comment)
   <blockquote>{{ $evaluation->comment }}</blockquote>
 @endif
+
+<div>
+  Kontaktuppgifter till {{ $opportunity->workplace->name }}:<br>
+  {{ $opportunity->display_contact_name }}<br>
+  {{ $opportunity->display_contact_phone }}<br>
+  {{ $opportunity->workplace->display_email }}
+  <address>
+    {{ nl2br($opportunity->display_address) }}
+  </address>
+</div>
