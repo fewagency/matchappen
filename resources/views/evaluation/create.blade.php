f7999aab646c33d8d86cc8b9e530fb8ad102286a
bjuppa <nilsved@gmail.com>
2016-08-29 11:42:52 +0200

    Add TODO for admin-contact in evaluation view

--- resources/views/evaluation/create.blade.php
+++ resources/views/evaluation/create.blade.php
@@ -2,5 +2,6 @@
 
 @section('content')
   <h1>{{ trans('evaluation.opportunity_evaluation_headline', ['opportunity' => $opportunity->name]) }}</h1>
+  <!-- TODO: display contact-info to admin for bigger problems that occurred during visit -->
   @include('evaluation.partials.evaluation_form')
 @endsection
