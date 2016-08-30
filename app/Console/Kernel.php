cc8a3cfd8afecee6fa3cc450939d521137206585
bjuppa <nilsved@gmail.com>
2016-08-30 10:10:53 +0200

    Make sure evaluation send jobs don't overlap

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('evaluations:send')
-            ->everyTenMinutes();
+        $schedule->command('evaluations:send')->everyTenMinutes()->withoutOverlapping();
     }
 }
