36356d87d15ab93d717a1bf5cfcb2ec2ee1df62f
bjuppa <nilsved@gmail.com>
2016-10-04 14:05:18 +0200

    Post reset token in hidden input

--- resources/views/auth/partials/reset_form.blade.php
+++ resources/views/auth/partials/reset_form.blade.php
@@ -1,6 +1,7 @@
 {{
 FluentForm::withAction(action('Auth\PasswordController@postReset'))->withValues(old())->withErrors($errors)
 ->withToken(csrf_token())
+->withHiddenInput('token', $token)
 ->containingInputBlock('email','email')->withLabel(trans('auth.labels.your_email'))->required()
 ->followedByPasswordBlock()->withLabel(trans('auth.labels.desired_password'))->required()
 ->followedByPasswordBlock('password_confirmation')->withLabel(trans('auth.labels.password_confirmation'))->required()
